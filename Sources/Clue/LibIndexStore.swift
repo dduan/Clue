cf8d8c696e3b76abb2ac779ef38ca2e1b14d96a3
Daniel Duan <daniel@duan.ca>
2021-12-29 00:43:58 -0800

    fixup! fixup! Build on Linux

--- Sources/Clue/LibIndexStore.swift
+++ Sources/Clue/LibIndexStore.swift
@@ -12,5 +12,9 @@ public func defaultPathToLibIndexStore() throws -> String {
     let outputData = output.fileHandleForReading.readDataToEndOfFile()
     let json = try JSONSerialization.jsonObject(with: outputData)
     let pathToSwift = ((json as! [String: Any])["paths"] as! [String: Any])["runtimeResourcePath"] as! String
+#if os(macOS)
+    return "\(Path(pathToSwift).parent.joined(with: "libIndexStore.dylib"))"
+#else
     return "\(Path(pathToSwift).parent.joined(with: "libIndexStore.so"))"
+#endif
 }
