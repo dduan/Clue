2e209ece5bef208b0f7cef9f5da5f19cf0eaa3b0
Daniel Duan <daniel@duan.ca>
2021-12-29 15:52:18 -0800

    Hook up interface with main() (#4)

--- Sources/Clue/USRQuery.swift
+++ Sources/Clue/USRQuery.swift
@@ -1,10 +1,12 @@
 import IndexStoreDB
 
+// TODO: this sholud be an enum with a case .explicit(usrs: [String])
 /// Information that help pinning down one or more USRs
 public struct USRQuery {
     var symbol: String
     var module: String?
     var symbolKind: IndexSymbolKind?
+    // TODO: var isSystem: Bool = false
 
     public init(symbol: String, module: String? = nil, symbolKind: IndexSymbolKind? = nil) {
         self.symbol = symbol
