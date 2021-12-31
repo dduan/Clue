565dbbc87080a144b4708f97df7080b4d9186c03
Daniel Duan <daniel@duan.ca>
2021-12-31 14:44:52 -0800

    Make Engine hold store states (#12)
    This patch makes it possible to run mulitple queries from a single store
    read.

--- Sources/Clue/ClueEngine.swift
+++ Sources/Clue/ClueEngine.swift
@@ -1,35 +1,61 @@
 import IndexStoreDB
 import Pathos
 
-public enum ClueEngine {
-    public enum Failure: Error {
-        case ambiguousSymbol([SymbolOccurrence])
-        case symbolNotFound(byName: String)
-        case symbolNotFound(byUSR: String)
-    }
-
-    static func loadIndexStore(_ query: Query.Store) throws -> IndexStoreDB {
-        guard let libIndexStore = try? IndexStoreLibrary(dylibPath: query.libIndexStore) else {
-            throw Query.Store.Failure.invalidLibIndexStore
+public struct ClueEngine {
+    let db: IndexStoreDB
+    let libPath: String
+    let storeLocation: StoreLocation
+    /// - throws: StoreInitializationError
+    public init(libIndexStorePath: String? = nil, _ storeLocation: StoreLocation) throws {
+        let libIndexStore: IndexStoreLibrary
+        do {
+            self.libPath = try libIndexStorePath ?? defaultPathToLibIndexStore()
+            libIndexStore = try IndexStoreLibrary(dylibPath: libPath)
+        } catch let error as StoreInitializationError {
+            throw error
+        } catch let error {
+            throw StoreInitializationError.invalidLibIndexStore(error)
         }
 
-        let storePath = try query.location.resolveIndexStorePath()
+        let storePath = try storeLocation.resolveIndexStorePath()
         guard let indexStore = try? IndexStoreDB(
             storePath: storePath,
             databasePath: "\(try Path.makeTemporaryDirectory())",
             library: libIndexStore
         ) else {
-            throw Query.Store.Failure.invalidIndexStore
+            throw StoreInitializationError.invalidIndexStore
         }
 
         indexStore.pollForUnitChangesAndWait()
 
-        return indexStore
+        self.storeLocation = storeLocation
+        self.db = indexStore
     }
 
+    public func execute(_ query: Query) throws -> Finding {
+        let role = Query.Role.specific(
+            role: query.role.positive.isEmpty ? .all : query.role.positive,
+            negativeRole: query.role.negative
+        )
+
+        let definition = try self.findDefinition(from: query.usr)
+        let result = db.occurrences(ofUSR: definition.symbol.usr, roles: role.positive)
+            .filter { !$0.roles.isSuperset(of: [.implicit, .definition]) }
+            .filter { $0.roles.isDisjoint(with: role.negative.union(.definition)) }
+
+        return .init(
+            libIndexStore: self.libPath,
+            storeLocation: self.storeLocation,
+            usrQuery: query.usr,
+            referenceRole: role,
+            definition: definition,
+            occurrences: result
+        )
+    }
+
+
     /// Find occurence for a definition associated with the symbolName, module, and kind.
-    static func inferReferenceQuerySymbol(
-        db: IndexStoreDB,
+    func inferReferenceQuerySymbol(
         symbolName: String,
         module: String?,
         kind: IndexSymbolKind?,
@@ -56,30 +82,8 @@ public enum ClueEngine {
         return candidates[0]
     }
 
-    public static func execute(_ query: Query) throws -> Finding {
-        // TODO: this is very inefficient for mulitple queries.
-        let db = try loadIndexStore(query.store)
-        let role = Query.Role.specific(
-            role: query.role.positive.isEmpty ? .all : query.role.positive,
-            negativeRole: query.role.negative
-        )
-
-        let definition = try self.findDefinition(db: db, from: query.usr)
-        let result = db.occurrences(ofUSR: definition.symbol.usr, roles: role.positive)
-            .filter { !$0.roles.isSuperset(of: [.implicit, .definition]) }
-            .filter { $0.roles.isDisjoint(with: role.negative.union(.definition)) }
-
-        return .init(
-            storeQuery: query.store,
-            usrQuery: query.usr,
-            referenceRole: role,
-            definition: definition,
-            occurrences: result
-        )
-    }
-
     /// Return a definition for the given USR query
-    static func findDefinition(db: IndexStoreDB, from query: Query.USR) throws -> SymbolOccurrence {
+    func findDefinition(from query: Query.USR) throws -> SymbolOccurrence {
         let definition: SymbolOccurrence
         switch query {
         case .explict(let usr):
@@ -91,7 +95,6 @@ public enum ClueEngine {
             definition = candidates[0]
         case .query(let symbol, let module, let kind, let isSystem, let strictSymbolLookup):
             definition = try self.inferReferenceQuerySymbol(
-                db: db,
                 symbolName: symbol,
                 module: module,
                 kind: kind,
@@ -101,5 +104,13 @@ public enum ClueEngine {
         }
 
         return definition
+    }
+}
+
+extension ClueEngine {
+    public enum Failure: Error {
+        case ambiguousSymbol([SymbolOccurrence])
+        case symbolNotFound(byName: String)
+        case symbolNotFound(byUSR: String)
     }
 }
--- Sources/Clue/Query.swift
+++ Sources/Clue/Query.swift
@@ -1,10 +1,8 @@
 public struct Query {
-    public let store: Query.Store
     public let usr: Query.USR
     public let role: Query.Role
 
-    public init(store: Query.Store, usr: Query.USR, role: Query.Role) {
-        self.store = store
+    public init(usr: Query.USR, role: Query.Role) {
         self.usr = usr
         self.role = role
     }
