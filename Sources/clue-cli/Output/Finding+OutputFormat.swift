8a9820eb45c9b32e731a49f532f9c10ce53e4135
Daniel Duan <daniel@duan.ca>
2022-01-01 14:39:20 -0800

    Implement ability to turn colors on and off (#15)

--- Sources/clue-cli/Output/Finding+OutputFormat.swift
+++ Sources/clue-cli/Output/Finding+OutputFormat.swift
@@ -1,12 +1,15 @@
 import Clue
+import IsTTY
 
 extension Finding {
     func description(for format: OutputFormat) -> String {
         switch format {
         case .automatic:
-            return self.readableOutput(colored: true) // TODO: `false` if not writing to a tty
+            return self.readableOutput(colored: IsTTY.standardOutput) // TODO: `false` if not writing to a tty
         case .readable:
             return self.readableOutput(colored: false)
+        case .colored:
+            return self.readableOutput(colored: true)
         case .csv:
             return self.csv()
         case .json:
