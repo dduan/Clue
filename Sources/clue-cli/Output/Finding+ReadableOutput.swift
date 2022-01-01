3fe1218651a54878294c298f5ccc517b728166b5
Daniel Duan <daniel@duan.ca>
2022-01-01 15:29:10 -0800

    Implement colored redable output (#16)

--- Sources/clue-cli/Output/Finding+ReadableOutput.swift
+++ Sources/clue-cli/Output/Finding+ReadableOutput.swift
@@ -1,20 +1,40 @@
-import Clue
 import Chalk
+import Clue
+import IndexStoreDB
+import Pathos
 
 extension Finding {
     func readableOutput(colored: Bool) -> String {
-        colored ? self.outputColored() : self.outputMono()
-    }
+        var lines = [String]()
+        var occurByFile = [String: [SymbolOccurrence]]()
+        for occur in [self.definition] + self.occurrences {
+            occurByFile[occur.location.path, default: []].append(occur)
+        }
 
-    private func outputColored() -> String {
-        self.occurrences
-            .map { "\($0.location.path):\($0.location.line, color: .green):\($0.location.utf8Column)" }
-            .joined(separator: "\n")
-    }
+        for file in occurByFile.keys {
+            let content = (try? Path(file).readUTF8String())?
+                .split(separator: "\n", omittingEmptySubsequences: false)
+                ?? []
+            lines.append(colored ? "\(file, color: .magenta)" : file)
+            for occur in occurByFile[file, default: []] {
+                let location = occur.location
+                let lineContent = content.count > location.line ? content[location.line - 1] : ""
+                if colored {
+                    let prefix = lineContent.prefix(location.utf8Column - 1)
+                    let nameToHighlight = self.definition.symbol.name.split(separator: "(")[0]
+                    let highlight = lineContent
+                        .dropFirst(location.utf8Column - 1)
+                        .prefix(nameToHighlight.count)
+                    let postfix = lineContent.dropFirst(location.utf8Column - 1 + nameToHighlight.count)
+                    lines.append("\(location.line, color: .green):\(location.utf8Column):\(prefix)\(highlight, color: .red)\(postfix)")
+                } else {
+                    lines.append("\(location.line):\(location.utf8Column):\(lineContent)")
+                }
+            }
 
-    private func outputMono() ->String {
-        self.occurrences
-            .map { $0.locationString }
-            .joined(separator: "\n")
+            lines.append("")
+        }
+
+        return lines.joined(separator: "\n")
     }
 }
