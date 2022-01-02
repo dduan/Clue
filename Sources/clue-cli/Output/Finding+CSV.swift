89ca314ee18b6f8f63ea466fe1b389f2715bc38b
Daniel Duan <daniel@duan.ca>
2022-01-01 17:01:02 -0800

    Implement CSV output format (#18)

--- Sources/clue-cli/Output/Finding+CSV.swift
+++ Sources/clue-cli/Output/Finding+CSV.swift
@@ -1,7 +1,26 @@
 import Clue
+import IndexStoreDB
 
 extension Finding {
     func csv() -> String {
-        "TODO: Implement CSV formatting"
+        let header = ["name", "usr", "kind", "roles", "path", "line", "column", "moduleName", "isSystem"]
+        let content = self.occurrences
+            .map { (occur: SymbolOccurrence) -> [String] in
+                 [
+                    occur.symbol.name,
+                    occur.symbol.usr,
+                    occur.symbol.kind.description,
+                    occur.roles.description,
+                    occur.location.path,
+                    occur.location.line.description,
+                    occur.location.utf8Column.description,
+                    occur.location.moduleName,
+                    occur.location.isSystem.description,
+                ]
+            }
+
+        return ([header] + content)
+            .map { $0.joined(separator: ",") }
+            .joined(separator: "\n")
     }
 }
