8e65a681d10bf4cecf85619ae23db83ba93458ba
Daniel Duan <daniel@duan.ca>
2022-01-18 21:19:09 -0800

    Improve ambiguous definition output (#31)
    Make each ambiguous definition multiline. Include USR.
    
    Close #30.

--- Sources/Clue/ClueEngine+Failure.swift
+++ Sources/Clue/ClueEngine+Failure.swift
@@ -13,14 +13,26 @@ extension ClueEngine.Failure: CustomStringConvertible {
         switch self {
         case .ambiguousSymbol(let occurrences):
             return "Found more than one symbol matching your input. "
-                + "Pick one from the following with more details (full name, --module, and/or --kind):\n"
+                + "Pick one from the following with more details (full name, --module, and/or --kind):\n\n"
                 + occurrences
-                    .map { "\($0.symbol.name) (\($0.symbol.kind)) at \($0.locationString)" }
-                    .joined(separator: "\n")
+                    .map { $0.errorDescription }
+                    .joined(separator: "\n\n")
         case .symbolNotFoundByName(let name):
             return "Could not find a symbol matching name '\(name)'"
         case .symbolNotFoundByUSR(let usr):
             return "Could not find a symbol matching USR '\(usr)'"
         }
+    }
+}
+
+extension SymbolOccurrence {
+    fileprivate var errorDescription: String {
+        """
+        | Name     | \(self.symbol.name)
+        | Module   | \(self.location.moduleName)
+        | Kind     | \(self.symbol.kind)
+        | USR      | \(self.symbol.usr)
+        | Location | \(self.locationString)
+        """
     }
 }
