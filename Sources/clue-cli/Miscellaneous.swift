7e7b504e73c5732dc9120c08d7354d037fb562e9
Daniel Duan <daniel@duan.ca>
2022-04-20 12:55:07 -0700

    Use swift-system-exit

--- Sources/clue-cli/Miscellaneous.swift
+++ Sources/clue-cli/Miscellaneous.swift
@@ -1,16 +1,14 @@
+import SystemExit
+
 #if canImport(Darwin)
 import func Darwin.fputs
-import func Darwin.exit
-import var Darwin.EXIT_FAILURE
 import var Darwin.stderr
 #else
 import func Glibc.fputs
-import func Glibc.exit
-import var Glibc.EXIT_FAILURE
 import var Glibc.stderr
 #endif
 
 func bail(_ message: String) -> Never {
     fputs(message, stderr)
-    exit(EXIT_FAILURE)
+    exit(with: .failure)
 }
