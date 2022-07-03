46d146c28d8599153a726a83e95139db302fe52c
Daniel Duan <daniel@duan.ca>
2022-07-02 17:21:50 -0700

    Implement FilePath output

--- Sources/clue-cli/Output/Finding+FilePaths.swift
+++ Sources/clue-cli/Output/Finding+FilePaths.swift
@@ -1,16 +1,21 @@
 import Clue
+import IndexStoreDB
 
 extension Finding {
-    func filePaths() -> String {
-        switch self.details {
-        case .find(_, let definition, let occurrences):
-        let paths = ([definition] + occurrences)
+    private func format(_ occurrences: [SymbolOccurrence]) -> String {
+        let paths = occurrences
             .map { $0.location.path }
         return Set(paths)
             .sorted()
             .joined(separator: "\n")
-        case .dump:
-            fatalError("Implement me") // TODO
+    }
+
+    func filePaths() -> String {
+        switch self.details {
+        case .find(_, let definition, let occurrences):
+            return format([definition] + occurrences)
+        case .dump(_, let definitions):
+            return format(definitions)
         }
     }
 }
